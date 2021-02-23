b150a750fad9c92cf1027a3ed87f3e42bc791231
Marta Kedzior <marta.kedzior@wp.pl>
2021-02-23 22:47:28 +0100

    Poprawka literówki

--- 01-types/zadanie1.py
+++ 01-types/zadanie1.py
@@ -7,4 +7,4 @@ distans = 120
 price_of_gas = 5.04
 price_of_trip = mileage_per_100 * distans/100 * price_of_gas
 
-print(f"koszt_wyprawy to {round(price_of_trip,2)}")
+print(f"koszt wyprawy to {round(price_of_trip,2)}")
