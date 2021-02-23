ad6d095695b2bc67649b03b8bcf731aea1d313ed
Marta Kedzior <marta.kedzior@wp.pl>
2021-02-23 22:45:59 +0100

    Typy logiczne i operatory logiczne

---
+++ 01-types/zadanie2.py
@@ -0,0 +1,25 @@
+#Typy logiczne i operatory logiczne
+
+#1. Czy 23 + 3 będzie się równać 15 + 12 ?
+print(23 + 3 == 15 + 12)
+
+#2. Czy dzielenie 29 przez 7 bez reszty wynosi 5?
+print(29 // 7 == 5)
+
+#3. Czy 27 podzielone przez 8 daje resztę 3?
+print(27 % 8 == 3)
+
+#4. Wyświetl True, jeżeli liczba jest parzysta.
+a = 46
+print(a % 2 == 0)
+
+#5. Czy 43 - 13 będzie się równać 11 + 12 ?
+print(43 - 13 == 11 + 12)
+
+#6. Czy dzielenie 129 przez 17 bez reszty wynosi 3?
+print(129 // 17 == 3)
+
+#7. Czy 247 podzielone przez 5 daje resztę 2?
+print(247 % 5 == 2)
+
+
