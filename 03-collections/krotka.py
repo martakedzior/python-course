345097efa7eada582fd92cb8260eae301becb6ee
Marta Kedzior <marta.kedzior@wp.pl>
2021-03-08 19:50:06 +0100

    Krotka

---
+++ 03-collections/krotka.py
@@ -0,0 +1,11 @@
+# Krotki
+
+# Krotka (tuple) jest uporządkowaną sekwencją elementów taką samą jak lista. Jedyną różnicą jest to,
+# że krotki są niemutowalne (niezmienne), podobnie jak typ string. Krotki po utworzeniu nie można modyfikować.
+# Krotki tworzymy za pomocą nawiasów (), a wartości są oddzielone przecinkami.
+
+empty_tuple = ()
+len(empty_tuple)
+
+num_tuple = (1, 2, 3, 4)
+num_tuple[2]
