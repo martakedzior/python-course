be250c95b6ac0ca78844755a78a4ac311a0fe698
Marta Kedzior <marta.kedzior@wp.pl>
2021-03-13 12:53:59 +0100

    Instructions - Excercise 1a

---
+++ 02-instructions/for_zadanie1a.py
@@ -0,0 +1,11 @@
+items = ['latarka', 'bidon', 'portfel', 'namiot']
+
+# for item in items:
+#     print('Pod namiot zabieram:', item)
+
+print(items[2])
+
+for index in range(4):
+    print('Pod namiot zabieram:', items[index])
+
+print("Great, we are ready!")
