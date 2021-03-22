3cbd25bbf8f7fb79ab975ffbfed49b26c226c1b0
Marta Kedzior <marta.kedzior@wp.pl>
2021-03-22 20:25:51 +0100

    Strings excercise 7

---
+++ 01-types/string_zadanie7.py
@@ -0,0 +1,7 @@
+# 7. Stwórz dwie dowolne zmienne przechowujące wartość liczbową i tekstową.
+# Użyj funkcji format(), by wyświetlić zdanie zawierające te wartości.
+
+variable_1 = 4535
+variable_2 = 'tekst'
+
+print("Zmienna pierwsza przechowuje liczbę {}, a zmienna druga przechowuje tekst: {}".format(variable_1, variable_2))
