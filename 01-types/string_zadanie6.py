441deff4b9fa4b95991e2f970aa2928c30ad635b
Marta Kedzior <marta.kedzior@wp.pl>
2021-03-22 22:10:28 +0100

    Strings excercise 6 - not working

---
+++ 01-types/string_zadanie6.py
@@ -0,0 +1,23 @@
+# 6. Przekopiuj zawartość import this do zmiennej.
+#
+# >>> import this
+
+#     Policz liczbę wystąpień słowa better.
+#     Usuń z tekstu symbol gwiazdki
+#     Zamień jedno wystąpienie explain na understand
+#     Usuń spacje i połącz wszystkie słowa myślnikiem
+#     Podziel tekst na osobne zdania za pomocą kropki
+
+#     Nie działa
+
+import this
+
+this_poem = this
+
+this_poem = str(this_poem)
+
+print(this_poem.count("better"))
+
+this_poem = this_poem.replace("*","")
+
+print(this_poem)
