a31c33fee6cab44112caa78e71e32d01cf521d51
Marta Kedzior <marta.kedzior@wp.pl>
2021-03-28 21:57:46 +0200

    Operations on files exercise 3 and 4

--- 06-operations_on_files/operations_on_files_exercise4.py
+++ 06-operations_on_files/operations_on_files_exercise4.py
@@ -1,33 +1,35 @@
-# 3▹ Wyświetl tylko 5 pierwszych linii
+# 4▹ Wyświetl 3 losowe cytaty
 
 from random import choice
+
 
 def select_quote(quotes):
     quote = choice(quotes)
     return quote
 
+
 def reformat(quote):
     quote_and_author = quote.split(' - ')
     return quote_and_author
 
+
 def show(quote):
-    print("Quote of the day is:")
-    print("*" * 50)
     print(quote[0].center(50))
     print(quote[1].strip().center(50))
-    print("*" * 50)
+
 
 def show_first_5(quotes):
     for q in quotes[0:5]:
         print(q)
 
+
+# Main program
 filename = "quotes.txt"
 with open(filename, 'r', encoding='utf-8') as fopen:
     lines = fopen.readlines()
 
-quote = select_quote(lines)
-quote = reformat(quote)
-show(quote)
-
-print('---------------------')
-show_first_5(lines)
+for i in range(0, 3):
+    quote = select_quote(lines)
+    quote = reformat(quote)
+    show(quote)
+    i += 1
