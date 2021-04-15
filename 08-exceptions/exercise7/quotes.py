38bec1bf7cdfdc9944bd3d09a69a985e9e5c4fb1
MartaKedzior <marta.kedzior@wp.pl>
2021-04-15 19:46:06 +0200

    Exeptions - exercise7

---
+++ 08-exceptions/exercise7/quotes.py
@@ -0,0 +1,37 @@
+from random import choice
+
+def select_quote(quotes):
+    quote = choice(quotes)
+    return quote
+
+def reformat(quote):
+    quote_and_author = quote.split(' - ')
+    return quote_and_author
+
+def show(quote):
+    print("Quote of the day is:")
+    print("*" * 50)
+    print(quote[0].center(50))
+    print(quote[1].strip().center(50))
+    print("*" * 50)
+
+def show_first_5(quotes):
+    for q in quotes[0:5]:
+        print(q)
+
+
+if __name__ == '__main__':
+    while True:
+        filename = input('Give me filename --> ')
+        try:
+            with open(filename, 'r') as fopen:
+                lines = fopen.readlines()
+            break
+        except FileNotFoundError:
+            print('File not found. Try again ;) ')
+            continue
+
+
+    quote = select_quote(lines)
+    quote = reformat(quote)
+    show(quote)
