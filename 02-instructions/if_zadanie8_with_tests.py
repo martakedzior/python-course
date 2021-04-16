ec9bce1c50f5342442c7dae654bcca0fbd4be9a8
MartaKedzior <marta.kedzior@wp.pl>
2021-04-16 23:30:35 +0200

    If statement - exercise 8

--- 02-instructions/if_zadanie8_with_tests.py
+++ 02-instructions/if_zadanie8_with_tests.py
@@ -1,11 +1,13 @@
 # Sortowanie. Trzy dowolne liczby podane przez użytkownika zapisz do trzech zmiennych.
 # Znajdź największą liczbę. Wyświetl liczby od największej do najmniejszej.
 
-print('Podaj proszę trzy dowolne liczby. Posortuje je od największej do najmniejszej.')
-user_input1 = float(input('Podaj pierwszą liczbę: '))
-user_input2 = float(input('Podaj drugą liczbę: '))
-user_input3 = float(input('Podaj trzecią liczbę: '))
+#print('Podaj proszę trzy dowolne liczby. Posortuje je od największej do najmniejszej.')
+# user_input1 = float(input('Podaj pierwszą liczbę: '))
+# user_input2 = float(input('Podaj pierwszą liczbę: '))
+# user_input3 = float(input('Podaj pierwszą liczbę: '))
 
+def equals(actual, expected):
+    assert actual == expected, f'Expected {expected}, got {actual}'
 
 def max_from_three(user_input1, user_input2, user_input3):
     max_number = 0
@@ -19,7 +21,9 @@ def max_from_three(user_input1, user_input2, user_input3):
     return max_number
 
 
-print(max_from_three(user_input1, user_input2, user_input3))
+equals(max_from_three(2, 34, 56), 56)
+equals(max_from_three(200, 34, 56), 200)
+equals(max_from_three(2, 340, 56), 340)
 
 
 
