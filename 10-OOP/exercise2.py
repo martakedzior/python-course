5a7c759d1125def3988af0f0a21434c58223b6d0
MartaKedzior <marta.kedzior@wp.pl>
2021-04-21 18:41:05 +0200

    Lesson 11 - Exercise2 OOP

---
+++ 10-OOP/exercise2.py
@@ -0,0 +1,26 @@
+# 2▹ Utwórz klasę dla storczyków. Storczyki z zasady mają różne kolory, pory kwitnienia, gatunki.
+# Utwórz dowolne atrybuty i metody. Dodaj atrybut wspólny dla wszystkich storczyków - królestwo roślin.
+#
+# Utwórz kilka storczyków z różnymi parametrami.
+
+
+class Orchid():
+    def __init__(self, kingdom, colour, season, breed):
+        self.kingdom = kingdom
+        self.colour = colour
+        self.season = season
+        self.breed = breed
+
+    def blossom(self):
+        return self.breed + " blossoms"
+
+
+orchid1 = Orchid("Kingdom of plants", "yellow", "winter", "orchid1")
+
+print(orchid1.colour, orchid1.season, orchid1.breed)
+
+orchid1 = Orchid("Kingdom of plants", "yellow", "winter", "orchid1")
+orchid2 = Orchid("Kingdom of plants", "fuchsia", "spring", "orchid2")
+orchid3 = Orchid("Kingdom of plants", "white", "summer", "orchid3")
+
+print(orchid3.blossom())
