11e431964a8d35c000d31495121b727b159b0657
MartaKedzior <marta.kedzior@wp.pl>
2021-05-10 23:00:51 +0200

    Mini game "Pogromca obcych"

--- 12_OOP_Blackjack/pogromca_obcych.py
+++ 12_OOP_Blackjack/pogromca_obcych.py
@@ -1,12 +1,20 @@
-class Player:
-  """ Gracz w grze strzelance. """
+import random
 
-  def blast(self, enemy):
-      print('Gracz razi wroga.\n')
-      enemy.die()
+class Player:
+    """ Gracz w grze strzelance. """
+
+    def blast(self, enemy):
+        print('Gracz razi wroga.\n')
+        occuracy = random.randint(1, 10)
+        if occuracy >= 5:
+            enemy.die()
+        else:
+            enemy.win()
+
 
 
 class Alien:
+
     """ Obcy w grze strzelance. """
 
     def die(self):
@@ -14,6 +22,10 @@ class Alien:
               'Walczyliśmy do końca. Nie, to nie koniec. Larwy moje jednoczcie się! \n',
               'O tak one pomszczą mnie pewnego dnia... \n',
               'Żegnaj, okrutny Wszechświecie! Umieeeraaam"')
+
+    def win(self):
+        print("Za słabo..... Obcy zjada Cię!!!\n",
+              "Mlask, mlask. Idę spać.... Zieeew!")
 
 
 if __name__ == '__main__':
