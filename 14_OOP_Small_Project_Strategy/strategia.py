cbc9b52210b83efca63214acbc93d1b193f1edd2
MartaKedzior <marta.kedzior@wp.pl>
2021-05-12 18:34:56 +0200

    Stage 3c

--- 14_OOP_Small_Project_Strategy/strategia.py
+++ 14_OOP_Small_Project_Strategy/strategia.py
@@ -1,28 +1,36 @@
-class Rycerz:
+class Wojownik:
     def __init__(self):
-        self._zycie = 60
         self._doswiadczenie = 0
 
     def __repr__(self):
-        return f"Rycerz: hp={self._zycie}, exp={self._doswiadczenie}"
+        nazwa = self.__class__.__name__
+        return f"{nazwa}: hp={self._zycie}, exp={self._doswiadczenie}"
+
+    def maszeruj(self, dystans):
+        nazwa = self.__class__.__name__
+        print(f"{nazwa}: Przeszedłem {dystans} m")
+        self._doswiadczenie += 0.02 * dystans
+
+
+class Rycerz(Wojownik):
+    def __init__(self):
+        super().__init__()
+        self._zycie = 60
 
     def maszeruj(self, dystans):
         print("Rycerz: Przeszedłem {} m".format(dystans))
-        self._doswiadczenie = self._doswiadczenie + dystans * 0.2
+        self._doswiadczenie += dystans * 0.2
 
     def atakuj(self):
         print("Rycerz: Machnąłem mieczem!")
         self._doswiadczenie += 0.3
 
 
-class Lucznik:
+class Lucznik(Wojownik):
     def __init__(self):
+        super().__init__()
         self._zycie = 40
-        self._doswiadczenie = 0
         self.liczba_strzal = 5
-
-    def __repr__(self):
-        return f"Lucznik: hp={self._zycie}, exp={self._doswiadczenie}"
 
     def maszeruj(self, dystans):
         print("Lucznik: Przeszedłem {} m".format(dystans))
