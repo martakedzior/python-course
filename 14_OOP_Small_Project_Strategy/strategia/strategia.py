20f21a72f1d372b244e47c66639c9ae68d8a5d75
MartaKedzior <marta.kedzior@wp.pl>
2021-05-12 18:54:47 +0200

    Stage 7

--- 14_OOP_Small_Project_Strategy/strategia/strategia.py
+++ 14_OOP_Small_Project_Strategy/strategia/strategia.py
@@ -18,6 +18,44 @@ def main():
     potahontas.atakuj()
     potahontas.atakuj()
 
+    rycerze = []
+
+    for i in range(4):
+        rycerze.append(Rycerz())
+
+    print(rycerze)
+
+    for rycerz in rycerze:
+        rycerz.maszeruj(2000)
+
+    rycerze.append(Rycerz())
+
+    for rycerz in rycerze:
+        rycerz.atakuj()
+
+    print("Oddział! Baczność!")
+    for rycerz in rycerze:
+        print(rycerz)
+
+
+    lucznicy = []
+
+    for l in range(3):
+        lucznicy.append(Lucznik())
+
+    armia = rycerze + lucznicy
+
+    print("Armia")
+    for wojownik in armia:
+        print(wojownik)
+
+    for wojownik in armia:
+        wojownik.atakuj()
+
+    print("Armia po ataku")
+    for wojownik in armia:
+        print(wojownik)
+
 
 if __name__ == "__main__":
     main()
